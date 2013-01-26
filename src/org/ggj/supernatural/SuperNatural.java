f26db6aaea48b7703462013b446a224812046028
Chris <zael@Hercules.(none)>
2013-01-25 21:31:41 -0500

    Add blank input template.

--- src/org/ggj/supernatural/SuperNatural.java
+++ src/org/ggj/supernatural/SuperNatural.java
@@ -5,12 +5,15 @@ import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 
 public class SuperNatural extends BasicGame {
 	
+	//Top Left is 0,0
+	//Images are rendered with coordinates at top left
 	Image plane = null;
-	Image land = null;
+	//Image land = null;
 	float x = 400;
 	float y = 300;
 	float scale = 1;
@@ -26,18 +29,34 @@ public class SuperNatural extends BasicGame {
 	public void init(GameContainer gc) 
 			throws SlickException {
 		plane = new Image("sprites/plane.png");
+		//land = new Image("sprites/land.jpeg");
 	}
  
 	@Override
 	public void update(GameContainer gc, int delta) 
 			throws SlickException     
 	{
+		Input input = gc.getInput();
 
+		if(input.isKeyDown(Input.KEY_A))
+        {
+        }
+ 
+        if(input.isKeyDown(Input.KEY_D))
+        {
+
+        }
+
+        if(input.isKeyDown(Input.KEY_W))
+        {
+
+        }
 	}
  
 	public void render(GameContainer gc, Graphics g) 
 			throws SlickException 
 	{
+		//land.draw(0, 0);
 		plane.draw(x, y, scale);
 	}
  
